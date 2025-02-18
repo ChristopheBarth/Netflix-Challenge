3c130dad1b29ebeac2a0fa52c310476d42cc47a4
THEOPHILE PICOLET <theophilepicolet@hotmail.fr>
2025-02-18 14:34:58 +0100

    insert into movie

--- server/database/schema.sql
+++ server/database/schema.sql
@@ -15,8 +15,8 @@ create table movie (
   synopsis TEXT not null,
   release_year YEAR  not null,
   duration TIME not null,
-  poster TEXT NOT NULL,
-  trailer TEXT NOT NULL
+  poster VARCHAR(255) NOT NULL,
+  trailer VARCHAR(255) NOT NULL
   -- foreign key(user_id) references user(id)
 );
 
@@ -35,7 +35,8 @@ movie_id int not null,
 user_id int not null
 );
 
--- insert into item(id, title, user_id)
--- values
---   (1, "Stuff", 1),
---   (2, "Doodads", 1);
+insert into movie (title, synopsis, release_year, duration, poster, trailer)
+VALUES
+  ("Matrix","Programmeur anonyme dans un service administratif le jour, Thomas Anderson devient Neo la nuit venue. Sous ce pseudonyme, il est l'un des pirates les plus recherchés du cyber-espace. A cheval entre deux mondes, Neo est assailli par d'étranges songes et des messages cryptés provenant d'un certain Morpheus. Celui-ci l'exhorte à aller au-delà des apparences et à trouver la réponse à la question qui hante constamment ses pensées : qu'est-ce que la Matrice ? Nul ne le sait, et aucun homme n'est encore parvenu à en percer les defenses. Mais Morpheus est persuadé que Neo est l'Elu, le libérateur mythique de l'humanité annoncé selon la prophétie. Ensemble, ils se lancent dans une lutte sans retour contre la Matrice et ses terribles agents...", "1999", "02:15:00",  "https://fr.web.img4.acsta.net/c_310_420/medias/04/34/49/043449_af.jpg", "https://www.youtube.com/watch?v=vKQi3bBA1y8&ab_channel=RottenTomatoesClassicTrailers" );
+
+  
