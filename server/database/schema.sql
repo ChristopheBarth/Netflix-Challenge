1e5afd628f3eca4c335589bcb934fa5073d08e37
THEOPHILE PICOLET <theophilepicolet@hotmail.fr>
2025-02-18 15:21:52 +0100

    modification faite commentaire, renommage des tables

--- server/database/schema.sql
+++ server/database/schema.sql
@@ -5,8 +5,7 @@ create table user (
   email varchar(255) not null unique,
   password varchar(255) not null,
   subscription BOOLEAN NOT NULL,
-  role VARCHAR(50) NOT NULL,
-  watchlist_id int not null
+  role VARCHAR(50) NOT NULL
 );
 
 create table movie (
@@ -17,7 +16,6 @@ create table movie (
   duration TIME not null,
   poster VARCHAR(255) NOT NULL,
   trailer VARCHAR(255) NOT NULL
-  -- foreign key(user_id) references user(id)
 );
 
 create table genre (
@@ -25,12 +23,12 @@ id int unsigned primary key auto_increment not null,
 name VARCHAR(255) not null
 );
 
-create table MovieGenre (
+create table movie_genre (
 movie_id INT NOT NULL,
 genre_id INT NOT NULL
 );
 
-create table Watchlist (
+create table watchlist (
 movie_id int not null,
 user_id int not null
 );
@@ -39,4 +37,3 @@ insert into movie (title, synopsis, release_year, duration, poster, trailer)
 VALUES
   ("Matrix","Programmeur anonyme dans un service administratif le jour, Thomas Anderson devient Neo la nuit venue. Sous ce pseudonyme, il est l'un des pirates les plus recherchés du cyber-espace. A cheval entre deux mondes, Neo est assailli par d'étranges songes et des messages cryptés provenant d'un certain Morpheus. Celui-ci l'exhorte à aller au-delà des apparences et à trouver la réponse à la question qui hante constamment ses pensées : qu'est-ce que la Matrice ? Nul ne le sait, et aucun homme n'est encore parvenu à en percer les defenses. Mais Morpheus est persuadé que Neo est l'Elu, le libérateur mythique de l'humanité annoncé selon la prophétie. Ensemble, ils se lancent dans une lutte sans retour contre la Matrice et ses terribles agents...", "1999", "02:15:00",  "https://fr.web.img4.acsta.net/c_310_420/medias/04/34/49/043449_af.jpg", "https://www.youtube.com/watch?v=vKQi3bBA1y8&ab_channel=RottenTomatoesClassicTrailers" );
 
-  
