568a0de425d4c68a47181bd74055d2cd4a7b532e
THEOPHILE PICOLET <theophilepicolet@hotmail.fr>
2025-02-18 12:30:20 +0100

    Create All Table

--- server/database/schema.sql
+++ server/database/schema.sql
@@ -1,7 +1,12 @@
 create table user (
   id int unsigned primary key auto_increment not null,
+  first_name VARCHAR(50) NOT NULL,
+  last_name VARCHAR(50) NOT NULL,
   email varchar(255) not null unique,
-  password varchar(255) not null
+  password varchar(255) not null,
+  subscription BOOLEAN NOT NULL,
+  role VARCHAR(50) NOT NULL,
+  watchlist_id int not null
 );
 
 create table movie (
@@ -15,7 +20,20 @@ create table movie (
   -- foreign key(user_id) references user(id)
 );
 
+create table genre (
+id int unsigned primary key auto_increment not null,
+name VARCHAR(255) not null
+);
 
+create table MovieGenre (
+movie_id INT NOT NULL,
+genre_id INT NOT NULL
+);
+
+create table Watchlist (
+movie_id int not null,
+user_id int not null
+);
 
 -- insert into item(id, title, user_id)
 -- values
