963d4e6d2ac7b3c089d6166414ba34b758512d6b
THEOPHILE PICOLET <theophilepicolet@hotmail.fr>
2025-03-06 16:29:06 +0100

    changes done

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -2,29 +2,35 @@ import { Link } from "react-router-dom";
 import "../styles/login.css";
 export default function Login() {
   return (
-    <div className="login">
-      <section className="login-page">
+    <section className="login">
+      <form className="login-page">
         <h1>Se Connecter</h1>
         <p>
           Vous pouvez vous connecter si vous possédez un compte, sinon vous
           pouvez en créez un <Link to="/signup">ici</Link>
         </p>
-        <div className="login-form">
-          <input
-            type="email"
-            name="account"
-            id="account"
-            placeholder="E-mail"
-          />
-          <input
-            type="password"
-            name="password"
-            id="password"
-            placeholder="Password"
-          />
+        <div className="container-form">
+          <div className="login-form">
+            <label htmlFor="email">E-mail : </label>
+            <input
+              type="email"
+              name="email"
+              id="email"
+              placeholder="Votre E-mail"
+            />
+          </div>
+          <div className="login-form">
+            <label htmlFor="password">Password : </label>
+            <input
+              type="password"
+              name="password"
+              id="password"
+              placeholder="Votre Password"
+            />
+          </div>
+          <button type="submit">Continuer</button>
         </div>
-        <button type="submit">Continuer</button>
-      </section>
-    </div>
+      </form>
+    </section>
   );
 }
