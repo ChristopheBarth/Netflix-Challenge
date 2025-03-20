c894952a46ad645929786e18edf952f148ed7be6
THEOPHILE PICOLET <theophilepicolet@hotmail.fr>
2025-03-20 15:30:05 +0100

    lien pour chaque film

--- client/src/components/MovieCards.tsx
+++ client/src/components/MovieCards.tsx
@@ -1,8 +1,12 @@
+import { Link } from "react-router-dom";
+
 export default function MovieCards({ movie }: MoviesProps) {
   return (
     <>
       <div className="card-movie-img">
-        <img src={movie.poster} alt="" />
+        <Link to={`/movies/${movie.id}`}>
+          <img src={movie.poster} alt="" />
+        </Link>
         <p className="movie-title">{movie.title}</p>
         <p className="movie-year">{movie.release_year}</p>
       </div>
