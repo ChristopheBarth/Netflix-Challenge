0a1984aa2ec27baafe05a2b3ec7688e4ddd9a11d
Alex Taranne <alextaranne@gmail.com>
2025-03-21 12:16:33 +0100

    fix css and redirection offer

--- client/src/pages/Catalogue.tsx
+++ client/src/pages/Catalogue.tsx
@@ -15,29 +15,13 @@ export default function Catalogue() {
   return (
     <>
       <div className="first-container">
-        <input
-          type="search"
-          id="site-search"
-          name="q"
-          placeholder="Rechercher"
-        />
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          height="24px"
-          viewBox="0 -960 960 960"
-          width="24px"
-          fill="#5f6368"
-          aria-hidden="true"
-        >
-          <path d="M784-120 532-372q-30 24-69 38t-83 14q-109 0-184.5-75.5T120-580q0-109 75.5-184.5T380-840q109 0 184.5 75.5T640-580q0 44-14 83t-38 69l252 252-56 56ZM380-400q75 0 127.5-52.5T560-580q0-75-52.5-127.5T380-760q-75 0-127.5 52.5T200-580q0 75 52.5 127.5T380-400Z" />
-        </svg>
         <img
           src="/Background_connection.jpg"
           className="img-container"
           alt=""
         />
         <button type="button" className="decouvrir-nos-offres">
-          Décrouvrir nos offres
+          <a href="#acces">Décrouvrir nos offres</a>
         </button>
       </div>
       <div className="show-movies">
@@ -76,7 +60,7 @@ export default function Catalogue() {
             <p className="bottom-free">Regardez en haute qualité</p>
             <p className="bottom-free">Gérez vos listes de films à voir</p>
           </div>
-          <div className="separation" />
+
           <div className="bottom-right">
             <h3>Accès Premium</h3>
             <p>Films illimités en haute qualité</p>
