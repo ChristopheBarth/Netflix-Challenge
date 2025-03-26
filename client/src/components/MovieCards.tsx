16a62fff00594e4c270821b628023dc2fa6d3bfe
Julien PICART <julienpicart01@gmail.com>
2025-03-26 15:15:53 +0100

    Fixed condition

--- client/src/components/MovieCards.tsx
+++ client/src/components/MovieCards.tsx
@@ -9,25 +9,25 @@ export default function MovieCards({ movie }: MoviesProps) {
     });
   };
   const { role, subscription } = useAuth();
-  console.info(subscription);
+
   return (
     <>
       <div className="card-movie-img">
         {role === "anonymous" ? (
           <Link to="/signup" onClick={scrollToTop}>
-            <img src={movie.poster} alt="" />
+            <img src={movie.poster} alt={movie.title} />
           </Link>
-        ) : subscription ? (
-          <Link to={`/movies/${movie.id}`} onClick={scrollToTop}>
-            <img src={movie.poster} alt="" />
+        ) : movie.premium && !subscription ? (
+          <Link to="/payment" onClick={scrollToTop}>
+            <img src={movie.poster} alt={movie.title} />
           </Link>
         ) : (
-          <Link to="/payment" onClick={scrollToTop}>
-            <img src={movie.poster} alt="" />
+          <Link to={`/movies/${movie.id}`} onClick={scrollToTop}>
+            <img src={movie.poster} alt={movie.title} />
           </Link>
         )}
+        <p className="movie-title">{movie.title}</p>
       </div>
-      <p className="movie-title">{movie.title}</p>
     </>
   );
 }
